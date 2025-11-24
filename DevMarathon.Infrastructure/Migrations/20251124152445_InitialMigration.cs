585db02a2d6b633401491dbb9645022bfe1c21f5
mohammad <mrabiee1375@gmail.com>
2025-11-24 19:10:08 +0330

    release project

---
+++ DevMarathon.Infrastructure/Migrations/20251124152445_InitialMigration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clean.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    phone_number = table.Column<string>(type: "text", nullable: false),
+                    verified = table.Column<bool>(type: "boolean", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    jalali_created_at = table.Column<string>(type: "character varying(30)", maxLength: 30, nullable: false),
+                    is_deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    jalali_date_key = table.Column<long>(type: "bigint", nullable: false),
+                    modify_date = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    is_enable = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
