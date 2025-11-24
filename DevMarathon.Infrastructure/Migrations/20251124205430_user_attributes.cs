0d3ae231d633a72e7c45b32e5feabe6b128ca75a
mohammad <mrabiee1375@gmail.com>
2025-11-25 00:27:22 +0330

    migrations

---
+++ DevMarathon.Infrastructure/Migrations/20251124205430_user_attributes.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clean.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_attributes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "phone_number",
+                table: "Users",
+                type: "character varying(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "phone_number",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(11)",
+                oldMaxLength: 11);
+        }
+    }
+}
